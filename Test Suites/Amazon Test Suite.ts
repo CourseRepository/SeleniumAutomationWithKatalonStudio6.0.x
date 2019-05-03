739fa9df7231d0347de45b1c346947b7f2fe49d8
Rathore <RahulSingh.Rathore@in.pega.com>
2019-05-03 07:24:57 -0700

    Amazon Search Grid Test

---
+++ Test Suites/Amazon Test Suite.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9b5c5e8a-ada7-491f-a0a7-623677092b7a</testSuiteGuid>
+   <testCaseLink>
+      <guid>edc984f1-6948-41a1-b0ac-843fc7555929</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WhileLoop/TC-AmazonSearch</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e15a552f-5e77-4dc4-874f-03890424fc6a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fae593e5-f797-4d68-b9ba-b0c5ef556ead</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
